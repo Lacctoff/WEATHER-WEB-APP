ba15557f3837dc2c9f6067a2b0e824d3f756e3f4
titosco <titobiloluwatella@gmail.com>
2024-04-09 15:16:35 +0100

    adding scripts

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-// const cityInput = "Lagos";
-const cityInput = document.querySelector(".time-zone");
+//Get current location of the user
+const button =document.querySelector("#get-location-button");
 
 
-const getCityCoordinates = () => {
-    cityName = cityInput.innerHTML; //input city name and remove whitespaces
-    if(!cityName){
-        return // return if empty        
-    }
-    return cityName;
+function failedToGet(){
+    alert('there was some issue')
+    console.log('there was some issue')
 }
-console.log(getCityCoordinates())
+
+button.addEventListener('click', ()=>{
+    navigator.geolocation.getCurrentPosition(gotLocation, failedToGet)
+})
+
+function gotLocation(position){
+    console.log(position)
+    
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    console.log(latitude,longitude)
+}
+
+
+
+// const cityInput = "Lagos";
+// const cityInput = document.querySelector(".time-zone");
+
+
+// const getCityCoordinates = () => {
+//     cityName = cityInput.innerHTML.trim(); //input city name and remove whitespaces
+//     if(!cityName){
+//         return // return if empty        
+//     }
+//     return cityName;
+// }
+// console.log(getCityCoordinates())
