e7d6b42966fe31f1ecf2e33c3a3504496914d886
titosco <titobiloluwatella@gmail.com>
2024-04-09 21:25:35 +0100

    adding script

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
 //Get current location of the user
 const button =document.querySelector("#get-location-button");
-
-
-function failedToGet(){
-    alert('there was some issue')
-    console.log('there was some issue')
-}
+// weather api key
+const API_KEY = "f004b7d5c5e56c9f2f923fda20760b4e";
 
 button.addEventListener('click', ()=>{
     navigator.geolocation.getCurrentPosition(gotLocation, failedToGet)
 })
+
+function failedToGet(){
+    alert('there was some issue')
+    console.log('there was some issue')
+};
 
 function gotLocation(position){
     console.log(position)
     
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
+
+    getData(latitude, longitude);
+
     console.log(latitude,longitude)
+
+    // console.log(getData)
+
+
 }
 
+function getData(latitude, longitude){
+    console.log('latitude is:', latitude)
+    console.log('longitude is:', longitude)
 
+    const GEOCODING_API_URL = `api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+    fetch(GEOCODING_API_URL)
+        .then(res =>{
+            if (!res.ok){
+                throw new Error('omo is all i can say cause e no work');
+            }
+            return res.json();
+        })
+        .catch(()=>{
+            alert('omo mad ohh, wetin you dey do!!')
+        })
+
+    // fetch(GEOCODING_API_URL).then(res => res.json()).then(data =>{
+    //     console.log(data)
+    // }).catch(() =>{
+    //     alert('omo mad error dey, go check am again!!')
+    // })
+}
 
 // const cityInput = "Lagos";
 // const cityInput = document.querySelector(".time-zone");
